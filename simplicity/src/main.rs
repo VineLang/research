bb23f59a765237c9bea90287e34adac10416ac7b
tjjfvi <t6@t6.fyi>
2024-10-10 12:08:43 -0400

    basic cli

--- simplicity/src/main.rs
+++ simplicity/src/main.rs
@@ -5,27 +5,57 @@ pub mod lexer;
 pub mod parser;
 pub mod scope;
 
+use std::{env::args, fs::read_to_string};
+
 use diagram::Diagram;
 use parser::SimplicityParser;
 
 fn main() {
-  let src = include_str!("../examples/nat.nets");
-  let system = SimplicityParser::parse(src).unwrap();
+  let path = args().nth(1).expect("must supply path");
+  let src = read_to_string(path).unwrap();
+  let system = SimplicityParser::parse(&src).unwrap();
 
-  let mut diagram = Diagram::default();
+  for rule in &system.rules {
+    let mut diagram = Diagram::default();
 
-  let net = &system.nets[0];
+    let a = &system.agents.defs[rule.a.agent];
+    let b = &system.agents.defs[rule.b.agent];
+    diagram.insert_free_ports(
+      rule.a.ports[1..].iter().copied(),
+      a.value.auxiliary.iter().map(|x| x.len()),
+    );
+    diagram.insert_free_ports(
+      rule.b.ports[1..].iter().copied(),
+      b.value.auxiliary.iter().map(|x| x.len()),
+    );
 
-  diagram.insert_free_ports(net.ports.iter().flatten().copied(), net.ports.iter().map(|x| x.len()));
+    for node in &rule.result {
+      let agent = &system.agents.defs[node.agent].value;
+      diagram.insert_agent(node.ports.iter().copied(), agent.auxiliary.iter().map(|x| x.len()));
+    }
 
-  for node in &net.nodes {
-    let agent = &system.agents.defs[node.agent].value;
-    diagram.insert_agent(node.ports.iter().copied(), agent.auxiliary.iter().map(|x| x.len()));
+    diagram.complete();
+    assert!(diagram.is_complete());
+
+    let simple = !diagram.is_contradictory();
+    println!("rule {}/{}: {}", a.name, b.name, if simple { "simple" } else { "non-simple" });
   }
 
-  diagram.complete();
-  assert!(diagram.is_complete());
+  for net in &system.nets {
+    let mut diagram = Diagram::default();
 
-  let consistent = !diagram.is_contradictory();
-  dbg!(consistent);
+    diagram
+      .insert_free_ports(net.ports.iter().flatten().copied(), net.ports.iter().map(|x| x.len()));
+
+    for node in &net.nodes {
+      let agent = &system.agents.defs[node.agent].value;
+      diagram.insert_agent(node.ports.iter().copied(), agent.auxiliary.iter().map(|x| x.len()));
+    }
+
+    diagram.complete();
+    assert!(diagram.is_complete());
+
+    let simple = !diagram.is_contradictory();
+    println!("net {}: {}", net.name, if simple { "simple" } else { "non-simple" });
+  }
 }
